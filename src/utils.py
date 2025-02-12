ad27029ad585150894e91d52c1c3f5c786ec5c4f
Ashutosh Gupta <ashutosh.1407@gmail.com>
2025-02-12 01:15:29 -0600

    predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,13 @@ from src.exception import  CustomException
 from src.logger import logging
  
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
